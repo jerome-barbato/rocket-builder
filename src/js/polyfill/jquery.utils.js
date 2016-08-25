0dcbd80f7424760fe5e7749d2cb45d3c71d16240
Paul Coudeville <paul@coudeville.info>
2016-08-25 19:28:01 +0200

    Merge branch 'comparator'
    # Conflicts:
    #	app/resources/src/template/blocks/comparator.phtml.twig
    #	app/resources/src/template/blocks/product.phtml.twig
    #	app/views/blocks/comparator.phtml.twig
    #	builder

--- src/js/polyfill/jquery.utils.js
+++ src/js/polyfill/jquery.utils.js
@@ -37,3 +37,25 @@ if(window.jQuery){
 
 if(window.jQuery && typeof $.fn.initialize == "undefined")
     $.fn.initialize = $.fn.each;
+
+/**
+ * Create Form Object from Array
+ * @see serializeArray
+ * @returns {{}}
+ */
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
