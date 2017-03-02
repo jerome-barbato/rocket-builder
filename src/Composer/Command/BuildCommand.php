73cd49301076502efbc12480c1b1b1d79abbf5c7
Paul Coudeville <paul@coudeville.info>
2017-03-02 12:37:37 +0100

    Rocket Composer Commands

--- src/Composer/Command/BuildCommand.php
+++ src/Composer/Command/BuildCommand.php
@@ -7,6 +7,7 @@ namespace Rocket\Composer\Command;
 
 use Composer\Command\BaseCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class BuildCommand extends BuilderCommand
@@ -14,20 +15,30 @@ class BuildCommand extends BuilderCommand
     protected function configure()
     {
         $this->setName('build');
+        $this->addOption('production', 'p', InputOption::VALUE_NONE, 'Optimize assets.');
+        $this->setHelp(<<<EOT
+Precompile assets from sources.
+EOT
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $args = $input->getArguments();
+        $output->writeln("<comment>Starting building...</comment>");
 
         if ( is_dir( $this->pkg_path ) ) {
+
             chdir( $this->pkg_path );
-            $options = count( $args ) ? $args[0] : '';
+
+            $options = $input->getOption('production') ? '-p' : '';
 
             if ( !is_dir( 'node_modules' ) ) {
+
+                $output->writeln("<comment>Node modules installation...</comment>");
                 $this->installNodeModules();
             }
 
+            $output->writeln("<comment>Gulp execution...</comment>");
             passthru( "gulp " . $options . " --color=always" );
         }
     }
--- src/Composer/Command/CreateCommand.php
+++ src/Composer/Command/CreateCommand.php
@@ -7,6 +7,7 @@ namespace Rocket\Composer\Command;
 
 
 use Composer\Command\BaseCommand;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -15,17 +16,23 @@ class CreateCommand extends BuildCommand
     protected function configure()
     {
         $this->setName('create');
+        $this->addArgument('module', InputArgument::IS_ARRAY, "Modules");
+        $this->setHelp(<<<EOT
+Create UI modules.
+EOT
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $args = $input->getArguments();
-
+        $args = $input->getArgument('module');
 
         if ( is_dir( $this->pkg_path ) ) {
             chdir( $this->pkg_path );
 
             if ( !is_dir( 'node_modules' ) ) {
+
+                $output->writeln("<comment>Node modules installation...</comment>");
                 $this->installNodeModules();
             }
 
@@ -35,6 +42,8 @@ class CreateCommand extends BuildCommand
                 array_shift( $args );
 
                 foreach ( $args as $arg ) {
+
+                    $output->writeln("<comment>Creating " . $type . " " . $arg . "...</comment>");
                     passthru( "gulp create --" . $type . " " . $arg . "  --color=always" );
                 }
             }
