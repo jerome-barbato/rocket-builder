9a7dcd03921bd9552099290c1891490cc723a248
jerome <jerome@metabolism.fr>
2017-03-31 07:48:12 -0700

    grid auto

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,9 @@
 
 'use strict';
 
-var gulp   = require('gulp'),
-    fs     = require('fs'),
-    config = require('./gulp/config');
+var gulp        = require('gulp'),
+    fs          = require('fs'),
+    config      = require('./gulp/config');
 
 
 /**
@@ -18,10 +18,10 @@ var gulp   = require('gulp'),
 
 var files = fs.readdirSync('./gulp');
 
-for (var i in files) {
-    if (files[i].split('.').pop() == 'js') {
+for (var i in files)
+{
+    if (files[i].split('.').pop() == 'js')
         require('./gulp/' + files[i]);
-    }
 }
 
 
@@ -30,23 +30,14 @@ for (var i in files) {
  *  main optimization build task
  */
 
-gulp.task('default', [
-    "views::clean",
-    "script::browser",
-    "script::vendor",
-    "script::app",
-    "style::compile",
-    "templates::compile"
-], function () {
-
-    if (config.watching_mode) {
-
+gulp.task('default', ["views::clean", "script::browser", "script::vendor", "script::app", "style::compile", "templates::compile"], function ()
+{
+    if (config.watching_mode)
+    {
         gulp.start("template::watch");
+
         gulp.watch(config.paths.watch.js_app, ["script::app"]);
-        gulp.watch(config.paths.watch.js_vendors, [
-            "script::vendor",
-            "templates::compile"
-        ]);
+        gulp.watch(config.paths.watch.js_vendors, ["script::vendor", "templates::compile"]);
         gulp.watch(config.paths.watch.sass, ["style::compile"]);
     }
 });
