3d14d56a40e863538db21c6674833b3a0167c3d5
jerome <jerome@metabolism.fr>
2018-01-08 06:38:14 -0800

    media query update

--- src/Gulp/sync.js
+++ src/Gulp/sync.js
@@ -11,7 +11,8 @@ var paths = [
 	'!'+config.paths.src+'/component/**/*.js',
 	'!'+config.paths.src+'/component/**/*.scss',
 	'!'+config.paths.src+'/component/**/*.twig',
-	'!'+config.paths.src+'/component/**/*.tpl'
+	'!'+config.paths.src+'/component/**/*.tpl',
+	'!'+config.paths.src+'/component/**/*__'
 ];
 
 function synchronize(done){
